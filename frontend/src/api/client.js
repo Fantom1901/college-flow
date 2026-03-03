09606c0729b0465e9a4276ac97bd42d5a201142a
fantom <tim19011974tim@gmail.com>
2026-03-03 22:53:03 +0700

    fix: total nginx api config

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -9,12 +9,10 @@ api.interceptors.request.use((config) => {
   const isDev = window.location.hostname === 'localhost';
   const realTgData = window.Telegram?.WebApp?.initData;
 
+  // Теперь мы шлем ЛИБО данные ТГ, ЛИБО заглушку. Заголовок БУДЕТ ВСЕГДА.
+  const authData = isDev ? '123456789' : (realTgData || 'NO_TG_DATA_FROM_FRONT');
 
-  const authData = isDev ? '123456789' : realTgData;
-
-  if (authData) {
-    config.headers['X-TG-Data'] = authData;
-  }
+  config.headers['X-TG-Data'] = authData;
 
   return config;
 });
