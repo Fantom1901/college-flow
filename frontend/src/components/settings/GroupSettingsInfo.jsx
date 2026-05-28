ae609dbd7f0618351183092b556e9a72ae8d6124
Nixa <tim19011974tim@gmail.com>
2026-05-28 21:05:08 +0700

    feat: add premium spring layout animations for group and personal settings

---
+++ frontend/src/components/settings/GroupSettingsInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './GroupSettingsInfo.module.scss';
+
+function GroupSettingsInfo() {
+    const content = (
+        <>
+        
+        </>
+    )
+    
+    return (content)
+}
+
+export default GroupSettingsInfo;
---
+++ frontend/src/components/settings/PersonalSettings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './PersonalSettings.module.scss';
+
+function PersonalSettings() {
+    const content = (
+        <>
+        
+        </>
+    )
+    
+    return (content)
+}
+
+export default PersonalSettings;
