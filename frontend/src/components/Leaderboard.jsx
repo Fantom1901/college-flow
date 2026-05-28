45ff32f3dc66c4acad96f082477a7d315e79afe0
Nixa <tim19011974tim@gmail.com>
2026-05-28 20:30:32 +0700

    feat: add Leaderboard top-5 with dockbar glass styling and neon effects

---
+++ frontend/src/components/Leaderboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Leaderboard.module.scss';
+
+function Leaderboard() {
+    const content = (
+        <>
+        
+        </>
+    )
+    
+    return (content)
+}
+
+export default Leaderboard;
