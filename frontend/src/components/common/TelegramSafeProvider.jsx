112a835147d91038f112c902c39a02ccd183b433
Nixa <tim19011974tim@gmail.com>
2026-05-31 19:08:16 +0700

    fix(front): Исправил telegramSafeProvider от автозаполнения IDE и проблем ввода фантомного scss моделя

--- frontend/src/components/common/TelegramSafeProvider.jsx
+++ frontend/src/components/common/TelegramSafeProvider.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import styles from './TelegramSafeProvider.module.scss';
+import { viewport, useSignal } from '@tma.js/sdk-react';
 
-function TelegramSafeProvider() {
-    const content = (
-        <>
-        
-        </>
-    )
-    
-    return (content)
-}
+const TelegramSafeProvider = ({ children }) => {
+  const contentTopInset = useSignal(viewport.contentSafeAreaInsetTop) || 0;
+
+  const topPadding = contentTopInset > 0 ? `${contentTopInset + 36}px` : '24px';
+
+  return (
+    <div
+      className="h-screen w-full p-3 flex flex-col justify-end overflow-hidden"
+      style={{ paddingTop: topPadding }}
+    >
+      {/* Стеклянная карточка приложения */}
+      <main className="main-glass w-full max-w-md flex-1 rounded-[40px] overflow-hidden relative flex flex-col px-4 shadow-2xl border border-white/10">
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export default TelegramSafeProvider;
