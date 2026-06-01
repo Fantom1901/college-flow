8bd8a2dd8e96a251f2429c02ba35eb4d50ccdede
Nixa <tim19011974tim@gmail.com>
2026-06-02 00:33:14 +0700

    feat(ui): внедрение импульсного степпера CyberStepper с динамической тактилкой и песочницы компонентов

---
+++ frontend/src/components/ui/feedback/CyberStepper.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './CyberStepper.module.scss';
+
+function CyberStepper() {
+    const content = (
+        <>
+        
+        </>
+    )
+    
+    return (content)
+}
+
+export default CyberStepper;
---
+++ frontend/src/views/admin/ComponentSandboxView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ComponentSandboxView.module.scss';
+
+function ComponentSandboxView() {
+    const content = (
+        <>
+        
+        </>
+    )
+    
+    return (content)
+}
+
+export default ComponentSandboxView;
